4ecd0ec427d4dad302d5b4c4804a384bed0821cd
Nuwaithu <nuwaithu@cloudsource.co.jp>
2015-04-09 08:33:37 +0630

    message

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OOOOOO
+//
+//  Created by Nu Wai Thu on 4/8/15.
+//  Copyright (c) 2015 clousource. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
